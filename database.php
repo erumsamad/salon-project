19965345fba9a049fcb7caf3d0915b27a22a6b9a
erumsamad <erumsamad.jinnah@gmail.com>
2026-07-26 09:27:28 +0500

    Update database.php

--- database.php
+++ database.php
@@ -1,1 +1,16 @@
 
+<?php
+
+$conn = new mysqli(
+    $_ENV['MYSQLHOST'],
+    $_ENV['MYSQLUSER'],
+    $_ENV['MYSQLPASSWORD'],
+    $_ENV['MYSQLDATABASE'],
+    (int)$_ENV['MYSQLPORT']
+);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
