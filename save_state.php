e197ee6ebdaac8b7718c2098e3f49a8e48ecac91
erumsamad <erumsamad.jinnah@gmail.com>
2026-07-30 20:00:56 +0500

    Update save_state.php

--- save_state.php
+++ save_state.php
@@ -5,12 +5,12 @@ header("Content-Type:application/json");
 //include "../database.php";
 include("database.php");
 
-$phone=$_POST['phone'];
+//$phone=$_POST['phone'];
 
-$step=$_POST['step'];
+//$step=$_POST['step'];
 
-//$phone = $_GET['phone'] ?? '';
-///$step = $_GET['step'] ?? '';
+$phone = $_GET['phone'] ?? '';
+$step = $_GET['step'] ?? '';
 
 $sql="REPLACE INTO customer_states
 (phone,current_step)
