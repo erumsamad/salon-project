1446b2f956c197857997360a169901fa0266c108
erumsamad <erumsamad.jinnah@gmail.com>
2026-08-11 19:34:32 +0500

    Update save_customer.php

--- save_customer.php
+++ save_customer.php
@@ -5,8 +5,16 @@ header("Content-Type:application/json");
 //include "../database.php";
 include("database.php");
 
- $phone=$_POST['phone'];
-//$phone = $_POST['phone'] ?? '';
+ //$phone=$_POST['phone'];
+$phone = $_POST['phone'] ?? '';
+
+if ($phone === '') {
+    echo json_encode([
+        "success" => false,
+        "error" => "Phone is required"
+    ]);
+    exit;
+}
 
 $sql="INSERT INTO customers(phone)
 VALUES('$phone')";
