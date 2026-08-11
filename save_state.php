ff24aa816a7add0039a5f2e8c43b7355feefc31d
erumsamad <erumsamad.jinnah@gmail.com>
2026-08-11 19:54:31 +0500

    Update save_state.php

--- save_state.php
+++ save_state.php
@@ -1,45 +1,40 @@
 <?php
 
-header("Content-Type:application/json");
+header("Content-Type: application/json");
 
-//include "../database.php";
 include("database.php");
 
-//$phone=$_POST['phone'];
-
-//$step=$_POST['step'];
-
 $phone = $_GET['phone'] ?? '';
-$step = $_GET['step'] ?? '';
+$step  = $_GET['step'] ?? '';
 
-$sql="REPLACE INTO customer_states
-(phone,current_step)
-
-VALUES
-
-('$phone','$step')";
-
-
-if($conn->query($sql)){
-
-echo json_encode([
-
-"success"=>true
-
-]);
-
+if ($phone === '' || $step === '') {
+    echo json_encode([
+        "success" => false,
+        "error" => "Phone or step is missing"
+    ]);
+    exit;
 }
 
-else{
+$sql = "INSERT INTO customer_states (phone, current_step)
+        VALUES ('$phone', '$step')
+        ON DUPLICATE KEY UPDATE
+        current_step = '$step'";
 
-echo json_encode([
+if ($conn->query($sql)) {
 
-"success"=>false
+    echo json_encode([
+        "success" => true,
+        "phone" => $phone,
+        "state" => $step
+    ]);
 
-]);
+} else {
 
+    echo json_encode([
+        "success" => false,
+        "error" => $conn->error
+    ]);
 }
-
 
 $conn->close();
 
