c6d7473b18322af59155270d494b1bd98aa30ae4
erumsamad <erumsamad.jinnah@gmail.com>
2026-08-21 18:44:01 +0500

    Update salon-hours.php

--- salon-hours.php
+++ salon-hours.php
@@ -1,9 +1,12 @@
 <?php
-
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-header("Content-Type: application/json");
+//header("Content-Type: application/json");
 
 include("database.php");
 
