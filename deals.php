378c1db1c09c9713a82343b69963c0b976f6e603
erumsamad <erumsamad.jinnah@gmail.com>
2026-08-23 03:16:18 +0500

    Update deals.php

--- deals.php
+++ deals.php
@@ -8,14 +8,6 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
-include("database.php");
-
-
-/*
-|--------------------------------------------------------------------------
-| OPTIONS
-|--------------------------------------------------------------------------
-*/
 
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
 
